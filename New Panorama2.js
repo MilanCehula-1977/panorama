2f2f0e4903f196c38a45a376e1551648b4bc4eeb
MilanCehula-1977 <milancehula@gmail.com>
2024-12-29 10:31:47 +0100

    Add files via upload

---
+++ New Panorama2.js
@@ -0,0 +1,43 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "New Panorama2_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 179.99999999999997,
+	        "maxtilt": 15.472817412625183,
+	        "minpan": 0,
+	        "mintilt": -15.472817412625183
+	    },
+	"id": "New Panorama2",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"New Panorama2_tiles/New Panorama2_t_1b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_1d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_1f_%0y_%0x.jpg"},"height":16473,"left":{"src":"New Panorama2_tiles/New Panorama2_t_1l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_1r_%0y_%0x.jpg"},"tilesize":515,"up":{"src":"New Panorama2_tiles/New Panorama2_t_1u_%0y_%0x.jpg"},"width":16473},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_2b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_2d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_2f_%0y_%0x.jpg"},"height":8237,"left":{"src":"New Panorama2_tiles/New Panorama2_t_2l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_2r_%0y_%0x.jpg"},"tilesize":515,"up":{"src":"New Panorama2_tiles/New Panorama2_t_2u_%0y_%0x.jpg"},"width":8237},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_3b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_3d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_3f_%0y_%0x.jpg"},"height":4119,"left":{"src":"New Panorama2_tiles/New Panorama2_t_3l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_3r_%0y_%0x.jpg"},"tilesize":515,"up":{"src":"New Panorama2_tiles/New Panorama2_t_3u_%0y_%0x.jpg"},"width":4119},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_4b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_4d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_4f_%0y_%0x.jpg"},"height":2060,"left":{"src":"New Panorama2_tiles/New Panorama2_t_4l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_4r_%0y_%0x.jpg"},"tilesize":515,"up":{"src":"New Panorama2_tiles/New Panorama2_t_4u_%0y_%0x.jpg"},"width":2060},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_5b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_5d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_5f_%0y_%0x.jpg"},"height":1030,"left":{"src":"New Panorama2_tiles/New Panorama2_t_5l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_5r_%0y_%0x.jpg"},"tilesize":515,"up":{"src":"New Panorama2_tiles/New Panorama2_t_5u_%0y_%0x.jpg"},"width":1030},{"back":{"src":"New Panorama2_tiles/New Panorama2_t_6b_%0y_%0x.jpg"},"down":{"src":"New Panorama2_tiles/New Panorama2_t_6d_%0y_%0x.jpg"},"front":{"src":"New Panorama2_tiles/New Panorama2_t_6f_%0y_%0x.jpg"},"height":515,"left":{"src":"New Panorama2_tiles/New Panorama2_t_6l_%0y_%0x.jpg"},"right":{"src":"New Panorama2_tiles/New Panorama2_t_6r_%0y_%0x.jpg"},"tilesize":515,"up":{"src":"New Panorama2_tiles/New Panorama2_t_6u_%0y_%0x.jpg"},"width":515}],"multilevel":true,"preview":{"src":"New Panorama2_tiles/New Panorama2_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 46.42,
+	        "pan": 90,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
